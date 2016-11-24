00d58e3cbce606819951f593549c00b04bd5e073
mauro89x <mauro.a89x@gmail.com>
2016-11-23 23:54:44 -0300

    Refactors in code for better readability

--- src/components/story_new.js
+++ src/components/story_new.js
@@ -1,34 +1,75 @@
-import React, { Component } from 'react';
+import _ from 'lodash';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createStory } from '../actions/index';
+import { Link } from 'react-router';
+
+const FIELDS =  {
+	title: {
+		type: 'input',
+		label: 'Title for story'
+	},
+	author: {
+		type: 'input',
+		label: 'Name'
+	},
+	content: {
+		type: 'textarea',
+		label: 'Content'
+	}
+};
 
 class StoryNew extends Component {
+	static contextTypes = {
+		router: PropTypes.object
+	}
+
+	renderField(fieldConfig, field) {
+		const fieldHelper = this.props.fields[field];
+		return(
+			<div className={`form-group ${fieldHelper.touched && fieldHelper.invalid ? 'has-danger' : ''}`}>
+				<label>{fieldConfig.label}</label>
+				<fieldConfig.type className="form-control" {...fieldHelper} />
+				<div className="text-help">
+					{fieldHelper.touched ? fieldHelper.error : ''}
+				</div>
+			</div>
+		);
+	}
+
+	onSubmit(props) {
+		this.props.createStory(props).
+			then(() => {
+				this.context.router.push('/');
+			})
+	}
+
 	render() {
-		const { fields: { title, author, content }, handleSubmit } = this.props;
+		const { handleSubmit } = this.props;
 		return (
-			<form onSubmit={handleSubmit(this.props.createStory)}>
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 				<h3>Create a new Story!</h3>
-				<div className="form-group">
-					<label>Title</label>
-					<input type="text" className="form-control" {...title} />
-				</div>
-
-				<div className="form-group">
-					<label>Author</label>
-					<input type="text" className="form-control"  {...author}/>
-				</div>
-
-				<div className="form-group">
-					<label>Content</label>
-					<textarea className="form-control" {...content} />
-				</div>
+				{_.map(FIELDS, this.renderField.bind(this))}
 				<button type="submit" className="btn btn-primary">Submit</button>
+				<Link to='/' className="btn btn-danger">Cancel</Link>
 			</form>
 		);
 	}
 }
 
+function validate(values) {
+	const errors = {};
+	
+	_.each(FIELDS, (type, field) => {
+		if(!values[field]) {
+			errors[field]  = `Enter a ${field}`;
+		}
+	});
+	return errors;
+}
+
 export default reduxForm({
 	form: 'StoryNew',
-	fields: ['title', 'author', 'content']
+	fields: _.keys(FIELDS),
+	validate
 }, null, { createStory })(StoryNew);
